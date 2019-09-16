7358c518535e473e965c19e13dfae97bb57d7bbe
Just-A-Fool <aedanscarn@gmail.com>
2019-09-16 13:11:28 -0600

    new function

--- name.js
+++ name.js
@@ -1,8 +1,11 @@
 'use strict';
-function createGreeting(name, age) {
-    const yearOfBirth 2019-age;
-    return (`Hi, my name is ${name}. I am ${age} years old. I was born in ${yearOfBirth}`);
+function yearOfBirth(age) {
+  return 2019 - age;
 }
 
-const greeting1 = createGreeting("Aedan", 21);
+function createGreeting(name, age) {
+  return (`Hi, my name is ${name}. I am ${age} years old. I was born in ${yearOfBirth(age)}`);
+}
+
+const greeting1 = createGreeting('Aedan', 21);
 console.log(greeting1);
