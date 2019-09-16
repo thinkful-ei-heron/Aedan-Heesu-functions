6e3c3cea5fad9a2350e7f8d97418f0c2b9a488cd
Just-A-Fool <aedanscarn@gmail.com>
2019-09-16 12:55:15 -0600

    createdGreeting fuction added

--- name.js
+++ name.js
@@ -1,1 +1,6 @@
-console.log('Hi, my name is Heesu Kang. I am 25 years old.');
+'use strict';
+function createGreeting() {
+  console.log('Hi, my name is Heesu Kang. I am 25 years old.');
+}
+
+createGreeting();
